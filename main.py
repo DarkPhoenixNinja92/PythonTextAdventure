efe935616ac36562bdeb53f6309596de16a1eabf
Shaun Pour <scpour@gmail.com>
2022-12-17 20:03:32 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -17,9 +17,33 @@ if btnPress == 'ladder':
     else:
         print('You slip and fall to the forest floor, breaking your legs. A hungry lion smells your blood and comes to make a meal of you.')
         print('You have died.')
-        quit
+        restart = input('Do you wish to play again? (y or n) ')
+        if restart == 'y':
+            pass
+        elif restart == 'n':
+            quit
 elif btnPress == 'south':
-    print('south')
+    print('You make your way south through the thorny underbrush. You walk until your feet ache and you can go no further. Over the horizon, to the northwest, you spy a tower jutting out of the landscape. To the east, you spy a small log cabin. Perhaps a sign of life?')
+    btnPress = input('Do you make for the tower, head for the cabin or find a soft patch of ground to lay down and rest? (tower, cabin, or rest) ')
+    if btnPress == 'tower':
+            print(btnPress)
+    elif btnPress == 'cabin':
+            print(btnPress)
+    elif btnPress == 'rest':
+            restRoll = random.randint(1,25)
+            if restRoll < 10:
+                print('Your rest passes uneventfully. You awaken the next morning feeling extremely well rested.')
+                btnPress = input('You prepare to set out again. Do you head for the tower or the cabin? (tower, cabin) ')
+            elif restRoll <= 10 and restRoll < 20:
+                print('You awaken to a rustling in the bushes. Something is observing you.')
+            else:
+                print('You feel a sharp pain in your throat. You briefly open your eyes only to see a cloaked figure run a knife across your neck. He swiftly departs and you slowly bleed to death.')
+                print('Game over!')
+                restart = input('Do you wish to play again? (y or n) ')
+                if restart == 'y':
+                    pass
+                elif restart == 'n':
+                    quit
 elif btnPress == 'east':
     print('east')
 elif btnPress == 'field':
