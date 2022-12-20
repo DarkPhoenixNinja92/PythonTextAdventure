de049d42f733c672b32698f94f441ebe5c1ad5c1
Shaun Pour <scpour@gmail.com>
2022-12-20 08:58:26 -0500

    Basic battle system working
    I added the basic features for the player's first turn in the battle system and the ability to roll for initiative at the start as well as kill the enemy and celebrate it. But the system can't currently handle a situation where the first attack does not immediately kill the enemy. Also, the enemy's first turn is not yet implemented.

--- battle.py
+++ battle.py
@@ -1,8 +1,53 @@
+import random
+battleState = 'passive'
 health = 100
 exp = 0
+speed = 50
+weaponDmg = 9
 level = 1
 encounteredEnemy = True
 # damage = weaponskill + level + weaponDmg - enemyArmor
 
 if encounteredEnemy == True:
     battleState = 'engaged'
+    
+while battleState == 'engaged':
+    firstRoll = True
+    
+    enemyName = 'Thug'
+    enemyLevel = 1
+    enemyHealth = 10
+    enemySpeed = 20
+    enemyDmg = random.randint(2,15)
+    
+    if firstRoll == True:
+        print('You encountered a thug!')
+        battleJoin = input('Do you wish to fight or flee? ')
+        if battleJoin == 'fight':
+            print('Roll for initiative! ')
+            playerInitiative = random.randint(1, 20)
+            enemyInitiative = random.randint(1, 20)
+            
+            print(f'You rolled {playerInitiative}')
+            print(f'Your opponent rolled {enemyInitiative}')
+            
+            if playerInitiative > enemyInitiative:
+                print('You go first!')
+                move = input ('Do you wish to attack or flee? ')
+                if move == 'attack':
+                    hitRoll = random.randint(1, 20)
+                    enemyDodge = random.randint(1, 20)
+                    if hitRoll > enemyDodge:
+                        print('You land a blow')
+                        damageDealt = weaponDmg + level
+                        enemyHealth = enemyHealth - damageDealt
+                        if enemyHealth <= 0:
+                            battleState = 'passive'
+                            print('The thug crumples to the ground dead and you raise your blade in triumph!')
+                    elif hitRoll < enemyDodge:
+                        print('The thug deftly dances around your blows, avoiding harm.')
+            elif playerInitiative < enemyInitiative:
+                print('The enemy goes first!')
+            elif playerInitiative == enemyInitiative:
+                print('The roll was a tie. You both reroll')
+                
